94dcb9274ae2b906f3bc84099c995f945a725a25
sanjaygithub007 <sanjaysinghjsg@gmail.com>
2018-02-08 01:32:58 +0530

    My first git repo

---
+++ src/corejava/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author sanjay
+ *
+ */
+package corejava;
---
+++ src/datadrivenframework/AddNumExcel.java
@@ -0,0 +1,44 @@
+package datadrivenframework;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.write.Number;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+
+public class AddNumExcel
+{
+	public static void main(String[] args) throws BiffException, IOException 
+	{
+		//Open existing file
+		File f=new File("adddata.xls");
+		Workbook rwb=Workbook.getWorkbook(f);
+		Sheet rsh=rwb.getSheet(0);
+		int nour=rsh.getRows();
+		
+		//Open same excel file for result writing
+		WritableWorkbook wwb=Workbook.createWorkbook(f,rwb);
+		WritableSheet wsh=wwb.getSheet(1);
+		
+		//Perform Addition
+		for(int i=1;i<nour;i++)
+		{
+			int x=Integer.parseInt(rsh.getCell(0, i).getContents());
+			int y=Integer.parseInt(rsh.getCell(0,i).getContents());
+			int z=x+y;
+			Number n=new Number(0,i,z);
+			wsh.addCell(n);
+			
+			// save excel sheet and close it
+			wwb.write();
+			wwb.close();
+			rwb.close();
+		}
+		
+	}
+}
+	
